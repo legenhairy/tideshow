cac41c525abf56c79ab5b08eece776b6720a5b07
Harry Zhang <harryzhang100@gmail.com>
2021-04-19 23:45:44 -0700

    app now renders with initial day and a week later for the starting dates, the months and days stored in a state object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
@@ -10,6 +10,29 @@ import DataTable from "./components/DataTable";
 import CsvDownloader from "react-csv-downloader";
 import Button from "react-bootstrap/Button";
 
+let startDay = new Date().getDate();
+let startMonth = new Date().getMonth() + 1;
+
+if (startDay < 10) {
+  startDay = "0" + startDay.toString();
+}
+
+//for default To date, add 7 to the initial date
+//i wonder if i pass the last day of the current month, will i move on to the next month number? yes, i get it!
+let day2 = new Date();
+day2.setDate(day2.getDate() + 7);
+let endMonth = day2.getMonth() + 1;
+
+const initialValues = {
+  start: startDay,
+  fromMonth: startMonth,
+  end: day2.getDate(),
+  toMonth: endMonth,
+};
+
+//the numbers for the months will correspond to their string representation,
+//i.e. 4 for the month will match up with the option with value=4, which is April
+
 function App() {
   const [tideData, setTideData] = useState({});
   const [tideHeight, setTideHeight] = useState([]); //break it into its own state array
@@ -19,7 +42,7 @@ function App() {
   const [userTime, setUserTime] = useState("lst_ldt"); //for the user time zone
   const [datum, setDatum] = useState("MLLW");
   const [station, setStation] = useState("9414290");
-  //const [fromDay, setFromDay] = useState('2020');
+  const [dates, setDates] = useState(initialValues);
   const [isLoading, setIsLoading] = useState(false);
   const [showChart, setShowChart] = useState(false); //for the rendering of the chart first time, then each state update just rerenders the chart
 
@@ -32,19 +55,16 @@ function App() {
     //on initial call, use the current date for the from date parameter
     const today = new Date();
 
-    let mon = today.getMonth() + 1;
-    //reformat the month and day - add a 0 in front of them if < 10 value
+    let mon = dates.fromMonth;
+    //reformat the month values in state - if month is < 10, attach a 0 in front so api can read it
     if (mon < 10) {
       mon = "0" + mon.toString();
     }
-    let day = today.getDate();
-    if (day < 10) {
-      day = "0" + day.toString();
-    }
 
-    let fromDate = today.getFullYear().toString() + mon + day;
-
+    let fromDate = today.getFullYear().toString() + mon + dates.start; //the updated fromDay
+    console.log("the from date is: " + fromDate);
     /*by default, set the end date 7 days after the fromDate*/
+    let toDate = today.getFullYear().toString() + mon + dates.end;
 
     //make the api call for the noaa tide prediction api
     const axioOptions = {
@@ -53,7 +73,7 @@ function App() {
       params: {
         station: `${station}`,
         begin_date: `${fromDate}`,
-        end_date: "20210410",
+        end_date: `${toDate}`,
         product: "predictions",
         datum: `${datum}`,
         units: `${unitParam}`,
@@ -65,7 +85,7 @@ function App() {
 
     axios
       .request(axioOptions)
-      .then(function (response) {
+      .then((response) => {
         console.log(response); //for testing purposes
         setIsLoading(false);
         setTideTable(response.data.predictions);
@@ -134,6 +154,7 @@ function App() {
           },
           scaleLabel: {
             display: true,
+            fontStyle: "bold",
             labelString: "Day/ Time of Tide",
           },
         },
@@ -177,6 +198,13 @@ function App() {
     }
   };
 
+  const handleDayChange = (e) => {
+    const { name, value } = e.target;
+    console.log("name", name);
+    console.log("value", value);
+    setDates({ ...dates, [name]: value });
+  };
+
   // const columns = [
   //   {
   //     id: "t",
@@ -207,143 +235,15 @@ function App() {
       <div className="grid-container">
         <header className="tide-title">TIDES</header>
         <div className="main-content">
-          {/* <div className="row">
-            <div>
-              <h2>Station Name</h2>
-              <select className='station-selector' defaultValue='9414290' onChange={changeStation}>
-                <option value="9414290">9414290 San Francisco</option>
-                <option value="9413745">9413745 Santa Cruz, Monterey Bay,CA</option>
-                <option value="9414275">9414275 Ocean Beach, Outer Coast, CA</option>
-                <option value="9414806">9414806 Sausalito, San Francisco, CA</option>
-              </select>
-            </div>
-            <div>
-              <h2 className='from-select'>From:</h2>
-              <select className='month-selector' defaultValue='Feb'>
-                <option value="Jan">January</option>
-                <option value="Feb">Feburary</option>
-                <option value="March">March</option>
-                <option value="April">April</option>
-                <option value="June">June</option>
-                <option value="July">July</option>
-                <option value="August">August</option>
-                <option value="September">September</option>
-                <option value="October">October</option>
-                <option value="November">November</option>
-                <option value="December">December</option>
-              </select>
-              <select className='day-selector' defaultValue='24'>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-              </select>
-            </div>
-            <div>
-              <h2 className='to-select'>To:</h2>
-              <select className='month-selector' defaultValue='Feb'>
-                <option value="Jan">January</option>
-                <option value="Feb">Feburary</option>
-                <option value="March">March</option>
-                <option value="April">April</option>
-                <option value="June">June</option>
-                <option value="July">July</option>
-                <option value="August">August</option>
-                <option value="September">September</option>
-                <option value="October">October</option>
-                <option value="November">November</option>
-                <option value="December">December</option>
-              </select>
-              <select className='day-selector' defaultValue='20'>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-              </select>
-            </div>
-          </div>
-          <div className="row">
-              <div>
-                <h3>Units</h3>
-                <select defaultValue='Meters' onChange={e => handleUnits(e)}>
-                  <option value='Meters'>Meters</option>
-                  <option value='Feet'>Feet</option>
-                </select>
-              </div>
-              
-              <div>
-                <h3>Timezone</h3>
-                <select className='timezone-selector' defaultValue='lst_ldt' onChange={e => changeTimeZone(e)}>
-                  <option value="gmt">gmt</option>
-                  <option value="lst">lst</option>
-                  <option value="lst_ldt">lst_ldt</option>
-                </select>
-              </div>
-
-              <div>
-                <h3>Datum</h3>
-                <select className='datum-selector' defaultValue='MLLW' onChange={changeDatum}>
-                  <option>MHHW</option>
-                  <option>MHW</option>
-                  <option>MTL</option>
-                  <option value="MLLW">MLLW</option>
-                </select>
-              </div>
-              <button className='search-tides' onClick={getData}>
-                {isLoading ? <Lottie options={defaultOptions} /> : 'GET YOUR TIDE'}
-              </button>
-          </div>  */}
-          {/* place the chart container here only if the button has been clicked */}
           {showChart ? (
             <div className="chart-container">
               <Line data={tideData} options={options} />
             </div>
           ) : null}
-          <div class="container">
+          <div class="container filter-container">
             <div class="row top-row">
               <div class="col-md-6 mb-3">
-                <label for="station">Station Name</label>
+                <label htmlFor="station">Station Name</label>
                 <select class="form-select shadow" onChange={changeStation}>
                   <option value="9414290">9414290 San Francisco</option>
                   <option value="9413745">
@@ -358,21 +258,31 @@ function App() {
                 </select>
               </div>
               <div class="col-md-3 mb-3">
-                <label for="from-month">From:</label>
-                <select class="form-select shadow mb-1" defaultValue="April">
-                  <option value="Jan">January</option>
-                  <option value="Feb">Feburary</option>
-                  <option value="March">March</option>
-                  <option value="April">April</option>
-                  <option value="June">June</option>
-                  <option value="July">July</option>
-                  <option value="August">August</option>
+                <label htmlFor="from-month">From:</label>
+                <select
+                  class="form-select shadow mb-1"
+                  name="fromMonth"
+                  onChange={handleDayChange}
+                  value={dates.fromMonth}
+                >
+                  <option value="1">January</option>
+                  <option value="2">Feburary</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">June</option>
+                  <option value="6">July</option>
+                  <option value="7">August</option>
                   <option value="September">September</option>
                   <option value="October">October</option>
                   <option value="November">November</option>
                   <option value="December">December</option>
                 </select>
-                <select class="form-select shadow" defaultValue="1">
+                <select
+                  class="form-select shadow"
+                  name="start"
+                  onChange={handleDayChange}
+                  value={dates.start}
+                >
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -405,21 +315,31 @@ function App() {
                 </select>
               </div>
               <div class="col-md-3 mb-3">
-                <label for="to-month">To:</label>
-                <select class="form-select shadow mb-1" defaultValue="April">
-                  <option value="Jan">January</option>
-                  <option value="Feb">Feburary</option>
-                  <option value="March">March</option>
-                  <option value="April">April</option>
-                  <option value="June">June</option>
-                  <option value="July">July</option>
-                  <option value="August">August</option>
-                  <option value="September">September</option>
-                  <option value="October">October</option>
+                <label htmlFor="to-month">To:</label>
+                <select
+                  class="form-select shadow mb-1"
+                  name="toMonth"
+                  onChange={handleDayChange}
+                  value={dates.toMonth}
+                >
+                  <option value="1">January</option>
+                  <option value="2">Feburary</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">June</option>
+                  <option value="6">July</option>
+                  <option value="7">August</option>
+                  <option value="8">September</option>
+                  <option value="9">October</option>
                   <option value="November">November</option>
                   <option value="December">December</option>
                 </select>
-                <select class="form-select shadow" defaultValue="10">
+                <select
+                  class="form-select shadow"
+                  name="end"
+                  onChange={handleDayChange}
+                  value={dates.end}
+                >
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -449,13 +369,14 @@ function App() {
                   <option value="27">27</option>
                   <option value="28">28</option>
                   <option value="29">29</option>
+                  <option value="30">30</option>
                 </select>
               </div>
             </div>
             {/* 2nd row with the other elements seperated out in col-3 pieces */}
             <div class="row bottom-row">
               <div class="col-md-4 mb-3">
-                <label for="units">Units</label>
+                <label htmlFor="units">Units</label>
                 <select
                   class="form-select shadow"
                   required
@@ -466,7 +387,7 @@ function App() {
                 </select>
               </div>
               <div class="col-md-4 mb-3">
-                <label for="timezone">Timezone</label>
+                <label htmlFor="timezone">Timezone</label>
                 <select
                   class="form-select shadow"
                   required
@@ -478,7 +399,7 @@ function App() {
                 </select>
               </div>
               <div class="col-md-4 mb-3">
-                <label for="timezone">Datum</label>
+                <label htmlFor="timezone">Datum</label>
                 <select
                   class="form-select shadow"
                   required
@@ -506,7 +427,6 @@ function App() {
           </div>
         </div>
         <div className="bottom-wave">
-          {" "}
           {/* wrap the animated wave with a container- looks better visually, otherwise it looks cut off*/}
           <Waves />
         </div>
