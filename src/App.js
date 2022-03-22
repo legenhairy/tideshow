2c1a1cb4ac2972d83591c616c3ec0ec4a9eb0847
Harry Zhang <harryzhang100@gmail.com>
2022-03-21 22:48:51 -0700

    rendering of day options simplified, chart container minor adjustment

--- src/App.js
+++ src/App.js
@@ -255,6 +255,20 @@ function App() {
     },
   };
 
+  //function to render the day option elements
+  function renderDays() {
+    let days = [];
+    for (let i = 1; i < 32; i++) {
+      days.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+
+    return days;
+  }
+
   return (
     <div className="App">
       {/* show this when the tideData is actually filled, after the api gets called (in place of the filters box?)*/}
@@ -333,37 +347,7 @@ function App() {
                     onChange={handleDayChange}
                     value={dates.start}
                   >
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
+                    {renderDays()}
                   </select>
                 </div>
                 <div class="col-md-3 mb-3">
@@ -394,36 +378,7 @@ function App() {
                     onChange={handleDayChange}
                     value={dates.end}
                   >
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
+                    {renderDays()}
                   </select>
                 </div>
               </div>
